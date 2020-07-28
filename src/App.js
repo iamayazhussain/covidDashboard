cbb08122618cdfd9fca34515a9fabe709d8431e1
Ayaz <iamayazhussain@gmail.com>
2020-07-28 22:09:13 +0530

    Navbar and footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Components/Home";
 import Blog from "./Components/Blog";
 import Rnd from "./Components/Rnd";
 import NavBar from "./Components/Common/NavigationBar";
+import Footer from "./Components/Common/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -15,10 +16,11 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/home" component={Home}></Route>
+          <Route path="/" component={Home}></Route>
           <Route path="/blog" component={Blog}></Route>
           <Route path="/rnd" component={Rnd}></Route>
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
--- src/Components/Common/NavigationBar/NavigationBar.js
+++ src/Components/Common/NavigationBar/NavigationBar.js
@@ -5,6 +5,59 @@ import { Link } from "react-router-dom";
 const NavigationBar = () => {
   return (
     <>
+      <header>
+        <div className="header-top">
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-8 col-lg-8 col-md-8 col-12">
+                <div className="header_left">
+                  <ul className="header_ul">
+                    <li>
+                      <a href="https://islengg.ac.in">
+                        <span className="fa fa-phone"></span> 040 - 12345678
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://islengg.ac.in">ALUMNI</a>
+                    </li>
+                    <li>
+                      <a href="https://islengg.ac.in">E-MAIL</a>
+                    </li>
+                    <li>
+                      <a href="https://islengg.ac.in">ONLINE FEE</a>
+                    </li>
+                    <li>
+                      <a href="https://islengg.ac.in">CONTACT</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                <div className="header_right">
+                  <ul className="header_ul">
+                    <li>
+                      <a href="https://islengg.ac.in">
+                        Follow us<span className="ti-facebook"></span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://islengg.ac.in">
+                        <span className="fa fa-linkedin-square"></span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://islengg.ac.in">
+                        <span className="fa fa-youtube-play"></span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
       <Navbar collapseOnSelect expand="md" bg="light" variant="light">
         <Navbar.Brand as={Link} to="/">
           <img
@@ -22,7 +75,7 @@ const NavigationBar = () => {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto"></Nav>
           <Nav>
-            <Link to="/home">
+            <Link to="/">
               <Nav.Link href="#home">Home</Nav.Link>
             </Link>
             <NavDropdown title="About" id="collasible-nav-dropdown">
